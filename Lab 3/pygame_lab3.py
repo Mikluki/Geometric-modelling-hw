2516138d2f5a5f89f4c841e64f37a64ba6b74fba
Your Name <you@example.com>
2022-05-06 23:31:11 +0300

    first 3d

--- Lab 3/pygame_lab3.py
+++ Lab 3/pygame_lab3.py
@@ -37,7 +37,7 @@ RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 
 scale = 100
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1000, 800
 screen_center = [WIDTH/2, HEIGHT/2]  # x, y
 
 pygame.display.set_caption("3D projection Lab#2")
@@ -62,36 +62,37 @@ projection_matrix = np.matrix([
 ])
 
 
-def Rx(angle):
-    Rx = np.matrix([
+def get_matrix_Rx(angle):
+    rx = np.matrix([
         [1,             0,              0, 0],
         [0, np.cos(angle), -np.sin(angle), 0],
         [0, np.sin(angle),  np.cos(angle), 0],
         [0,             0,              0, 1]])
 
-    return Rx
+    return rx
 
 
-def Ry(angle):
-    Ry = np.matrix([
-        [np.cos(angle), 0,  np.sin(angle), 0],
-        [0,             1,              0, 0],
-        [0,-np.sin(angle),  np.cos(angle), 0],
-        [0,             0,              0, 1]])
+def get_matrix_Ry(angle):
+    ry = np.matrix([
+        [np.cos(angle),             0,  np.sin(angle), 0],
+        [0            ,             1,              0, 0],
+        [-np.sin(angle),            0,  np.cos(angle), 0],
+        [0            ,             0,              0, 1]])
 
-    return Ry
+    return ry
 
 
-def Rz(angle):
-    Rz = np.matrix([
+def get_matrix_Rz(angle):
+    rz = np.matrix([
         [0, np.cos(angle), -np.sin(angle), 0],
         [0, np.sin(angle),  np.cos(angle), 0],
         [0,             0,              1, 0],
         [0,             0,              0, 1]])
 
-    return Rz
+    return rz
 
-def translation_matrix(tx, ty,tz):
+
+def get_matrix_Tr(tx, ty, tz):
     translation_matrix = np.matrix([
         [1, 0, 0, tx],
         [0, 1, 0, ty],
@@ -109,10 +110,12 @@ def drawline(point1, point2):
 
 # ============= Classes ==================
 class Point():
-    def __init__(self, x, y):
-        self.vec = np.matrix([[x], [y], [1]])
+    def __init__(self, x, y, z):
+        self.vec = np.matrix([[x], [y], [z], [1]])
         self.x = self.vec[0, 0]
         self.y = self.vec[1, 0]
+        self.z = self.vec[2, 0]
+
 
     # def __add__(self, other_point):
 
@@ -120,74 +123,105 @@ class Point():
         self.vec = matrix @ self.vec
         self.x = self.vec[0, 0]
         self.y = self.vec[1, 0]
+        if len(self.vec) >= 3:
+            self.z = self.vec[2, 0]
+
         return self
 
-
+# print(Rz(np.pi/2)@a.vec)
 class Wheel:
-    def __init__(self, radius, x=screen_center[0], y=screen_center[1], color='black'):
+    def __init__(self, radius, x=0, y=0, z=0, color='black'):
 
         self.radius = radius
         self.diag_coef = radius/np.sqrt(2)
         self.color = color
 
-        self.center = Point(x, y)
+        self.center = Point(x, y, z)
         self.cirle_points = []
-        self.cirle_points.append( Point(self.center.x, self.center.y + self.radius ))
-        self.cirle_points.append( Point(self.center.x + self.diag_coef, self.center.y + self.diag_coef))
-        self.cirle_points.append( Point(self.center.x + self.radius, self.center.y ))
-        self.cirle_points.append( Point(self.center.x + self.diag_coef, self.center.y - self.diag_coef))
-        self.cirle_points.append( Point(self.center.x, self.center.y - self.radius ))
-        self.cirle_points.append( Point(self.center.x - self.diag_coef, self.center.y - self.diag_coef))
-        self.cirle_points.append( Point(self.center.x - self.radius, self.center.y ))
-        self.cirle_points.append( Point(self.center.x - self.diag_coef, self.center.y + self.diag_coef))
+        self.cirle_points.append( Point(self.center.x, self.center.y + self.radius, self.center.z ))
+        self.cirle_points.append( Point(self.center.x + self.diag_coef, self.center.y + self.diag_coef,\
+            self.center.z))
+        self.cirle_points.append( Point(self.center.x + self.radius, self.center.y, self.center.z ))
+        self.cirle_points.append( Point(self.center.x + self.diag_coef, self.center.y - self.diag_coef,\
+            self.center.z))
+        self.cirle_points.append( Point(self.center.x, self.center.y - self.radius, self.center.z ))
+        self.cirle_points.append( Point(self.center.x - self.diag_coef, self.center.y - self.diag_coef,\
+            self.center.z))
+        self.cirle_points.append( Point(self.center.x - self.radius, self.center.y, self.center.z ))
+        self.cirle_points.append( Point(self.center.x - self.diag_coef, self.center.y + self.diag_coef,\
+            self.center.z))
 
         self.points = list(self.cirle_points)
         self.points.append(self.center)
 
-        #self.cirle_points.append(  )
 
     def translate_2_origin(self,):
         for i in range(len(self.points)):
-            self.points[i].vecprod(
-                translation_matrix(-self.center.x, -self.center.y))
+            self.points[i].vecprod(get_matrix_Tr(-self.center.x, -self.center.y, -self.center.z))
         return self
 
-    def translate(self, tx, ty):
+    def translate(self, tx, ty, tz):
         for i in range(len(self.points)):
-            self.points[i].vecprod(translation_matrix(tx,ty))
+            self.points[i].vecprod(get_matrix_Tr(tx, ty, tz))
         return self
 
-    def rotate(self, angle):
-        cx, cy = self.center.x, self.center.y
+# ====================== Rotation ======================
+    def rotate_x(self, angle):
+        cx, cy, cz = self.center.x, self.center.y, self.center.z
         self.translate_2_origin()
         for i in range(len(self.points)):
-            self.points[i].vecprod(rotation_matrix(angle))
-        self.translate(cx, cy)
+            self.points[i].vecprod(get_matrix_Rx(angle))
+        self.translate(cx, cy, cz)
         return self
 
+    def rotate_y(self, angle):
+        cx, cy, cz = self.center.x, self.center.y, self.center.z
+        self.translate_2_origin()
+        for i in range(len(self.points)):
+            self.points[i].vecprod(get_matrix_Ry(angle))
+        self.translate(cx, cy, cz)
+        return self
+
+    def rotate_z(self, angle):
+        cx, cy, cz = self.center.x, self.center.y, self.center.z
+        self.translate_2_origin()
+        for i in range(len(self.points)):
+            self.points[i].vecprod(get_matrix_Rz(angle))
+        self.translate(cx, cy, cz)
+        return self
+
+
+# ====================== Priject and draw ======================
+    def project_to_screen(self):
+        for i in range(len(self.points)):
+            # print(self.points[i].vec, 'mult to\n', projection_matrix, '  n={}\n\n'.format(i))
+            projection2d = projection_matrix @ self.points[i].vec
+        return projection2d
 
 
     def draw(self):
+        self.project_to_screen()
         for i in range(len(self.cirle_points)):
             drawline(self.points[i], (self.points[(i+1) % len(self.cirle_points)]))
             drawline(self.points[i], (self.center))
+
 
 
 if __name__ == '__main__':
     '''Main method'''
 
 
-    # Keyboard:
-    # Collect events until released
-    listener = keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release)
-    listener.start()
+    # # Keyboard:
+    # # Collect events until released
+    # listener = keyboard.Listener(
+    #     on_press=on_press,
+    #     on_release=on_release)
+    # listener.start()
 
     FPS = 30
 
     translation = (0.00,0)
-    rotation = 0.000
+    rotation = 0.001
 
     # ============= Window and Draw ==================
     pygame.init()
@@ -199,6 +233,7 @@ if __name__ == '__main__':
     screen.fill(WHITE)
     screen.blit(bg, (0,0))
 
+    # ============= Create object ==================
     w = Wheel(100, x=screen_center[0], y=screen_center[1])
 
     while True:
@@ -213,30 +248,14 @@ if __name__ == '__main__':
                     exit()
 
         # update stuff
-        w.translate(*translation)
-        w.rotate(rotation)
+        # w.translate(*translation)
+        w.rotate_x(0.001)
+        w.rotate_y(0.002)
+        # w.rotate_z(0.001)
+
 
         w.draw()
 
 
         pygame.display.update()
 
-
-
-
-
-
-
-
-        # if direction == 0:
-        #     translation = (-0.2,0)
-        #     rotation = - 0.001
-        # if direction == 1:
-        #     translation = (0.2,0)
-        #     rotation =  0.001
-        # if direction == 2:
-        #     translation = (0,-0.2)
-        #     rotation = 0
-        # if direction == 3:
-        #     translation = (0.0,0.2)
-        #     rotation = 0
