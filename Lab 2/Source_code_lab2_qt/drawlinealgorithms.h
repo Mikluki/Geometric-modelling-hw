898e22207b91f5f427ebb0be6e10e42d2fa5004f
Your Name <you@example.com>
2022-05-06 20:41:11 +0300

    v

--- Lab 2/Source_code_lab2_qt/drawlinealgorithms.h
+++
@@ -1,8 +0,0 @@
-#ifndef DRAWLINEALGORITHMS_H
-#define DRAWLINEALGORITHMS_H
-
-#include <QPixmap>
-
-void Drawline(int ax,int ay,int cx,int cy,QRgb color, QImage* img);
-
-#endif // DRAWLINEALGORITHMS_H
